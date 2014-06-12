02f4b9a7795abccd96925e45e6aa3073cbf60dd8
dledee1995 <devaunteledee@DeVauntes-MacBook-Pro.local>
2014-06-12 18:22:12 -0400

    committing project 2 part 2 Listing

---
+++ com.ledee.project2part2/Resources/data.js
@@ -0,0 +1,25 @@
+
+var teams = {
+	
+	"nbaTeams" :{
+		"headTitle": "NBA teams",
+		"footTitle" : "NBA Footer",
+		"teams": [
+	{Title : "Heat",desc: "They are the miami Heat."}, 
+	{Title : "Spurs",desc: "They are the San antonio Spurs"},
+	{Title : "Lakers",desc: "They are the Los Angeles Lakers"}
+	],
+	},
+	"nflTeams" :{
+		"headTitle": "NFL teams",
+		"footTitle" : "NFL Footer",
+		"teams": [
+	{Title : "Panthers",desc: "They are the Carolina Panthers."}, 
+	{Title : "Giants",desc: "They are the New York Giants."},
+	{Title : "Broncos",desc: "They are the Denver Broncos."}
+	],
+}
+};
+
+exports.data = teams;
+	
